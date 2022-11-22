4566c87ba3226ca24aebad6d70f2c24411cad4b0
kevin moreno jaimes <kevinmoreno2106@gmail.com>
2022-11-22 17:25:06 -0500

    cors

--- src/main/java/com/upc/ReservaRecursos/ReservaRecursosApplication.java
+++ src/main/java/com/upc/ReservaRecursos/ReservaRecursosApplication.java
@@ -2,6 +2,9 @@ package com.upc.ReservaRecursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ReservaRecursosApplication {
@@ -10,4 +13,13 @@ public class ReservaRecursosApplication {
 		SpringApplication.run(ReservaRecursosApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+			}
+		};
+	}
 }
